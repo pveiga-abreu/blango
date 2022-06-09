0307c7aabeb4941141158bf35de9e70695ceb72c
Paulo Abreu <pveiga.abreu@gmail.com>
2022-06-09 19:41:51 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -30,7 +30,12 @@ class Dev(Configuration):
 	# Register definition
 	AUTH_USER_MODEL = "blango_auth.User"
 	EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+	SITE_ID = 1
 	ACCOUNT_ACTIVATION_DAYS = 7
+	ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+	ACCOUNT_EMAIL_REQUIRED = True
+	ACCOUNT_USERNAME_REQUIRED = False
+	ACCOUNT_AUTHENTICATION_METHOD = "email"
 
 	# Application definition
 	ALLOWED_HOSTS = values.ListValue(["localhost", "0.0.0.0", ".codio.io"])
@@ -51,12 +56,17 @@ class Dev(Configuration):
 			'django.contrib.contenttypes',
 			'django.contrib.sessions',
 			'django.contrib.messages',
+			"django.contrib.sites",
 			'django.contrib.staticfiles',
 			'blango_auth',
 			'blog',
 			'crispy_forms',
 			'crispy_bootstrap5',
 			"debug_toolbar",
+			"allauth",
+			"allauth.account",
+			"allauth.socialaccount",
+			"allauth.socialaccount.providers.google",
 	]
 
 	MIDDLEWARE = [
